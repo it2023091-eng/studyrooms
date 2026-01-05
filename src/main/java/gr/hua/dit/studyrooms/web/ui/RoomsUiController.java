5d2fb0cbb02ca14af21864ba8b567192ae591c29
it2024149 <it2024149@hua.gr>
2026-01-05 20:09:04 +0200

    Reattach project and update studyrooms

---
+++ src/main/java/gr/hua/dit/studyrooms/web/ui/RoomsUiController.java
@@ -0,0 +1,23 @@
+package gr.hua.dit.studyrooms.web.ui;
+
+import gr.hua.dit.studyrooms.core.service.StudyRoomService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RoomsUiController {
+
+    private final StudyRoomService studyRoomService;
+
+    public RoomsUiController(StudyRoomService studyRoomService) {
+        this.studyRoomService = studyRoomService;
+    }
+
+    // /rooms -> εμφανίζει λίστα με τα δωμάτια
+    @GetMapping("/rooms")
+    public String showRooms(Model model) {
+        model.addAttribute("rooms", studyRoomService.getAllRooms());
+        return "rooms";   // θα ψάξει το rooms.html στα templates
+    }
+}
