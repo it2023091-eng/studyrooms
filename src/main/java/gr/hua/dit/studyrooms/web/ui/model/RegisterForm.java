9fefac49f45d6ef1d7fde466c841ede48d7efc69
it2023091-eng <it2023091@hua.gr>
2026-01-11 20:59:29 +0200

    Update RegisterForm.java

--- src/main/java/gr/hua/dit/studyrooms/web/ui/model/RegisterForm.java
+++ src/main/java/gr/hua/dit/studyrooms/web/ui/model/RegisterForm.java
@@ -1,6 +1,5 @@
 package gr.hua.dit.studyrooms.web.ui.model;
 
-
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
